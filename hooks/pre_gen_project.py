f6011b88a05f85cc7b5fe489f142afe57eae0ae8
Paul Steffen <46781050+PaulSteffen-betclic@users.noreply.github.com>
2023-09-30 16:26:50 +0000

    check package_name when creating project

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -12,7 +12,7 @@ logo = """
 print(logo)
 print("Validation of template values...")
 
-package_name = '{{ cookiecutter.python_package_name }}'
+package_name = '{{ cookiecutter.package_name }}'
 valid_python_name = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 if not re.match(valid_python_name, package_name):
     print(f"ERROR: '{package_name}' is not a valid Python module name.")
