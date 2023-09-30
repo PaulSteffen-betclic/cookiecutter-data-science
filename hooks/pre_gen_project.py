8dad76305807cac49e643ca8c0b8ea9ed43d3d3c
Paul Steffen <46781050+PaulSteffen-betclic@users.noreply.github.com>
2023-09-30 16:24:32 +0000

    check package_name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,22 +1,21 @@
-def deprecation_warning():
-    print("""
+import re
+import sys
 
-=============================================================================
-*** DEPRECATION WARNING ***
+logo = """
+██████  ███████ ████████  ██████ ██      ██  ██████ 
+██   ██ ██         ██    ██      ██      ██ ██      
+██████  █████      ██    ██      ██      ██ ██      
+██   ██ ██         ██    ██      ██      ██ ██      
+██████  ███████    ██     ██████ ███████ ██  ██████ 
+                                                    
+"""
+print(logo)
+print("Validation of template values...")
 
-Cookiecutter data science is moving to v2 soon, which will entail using
-the command `ccds ...` rather than `cookiecutter ...`. The cookiecutter command
-will continue to work, and this version of the template will still be available.
-To use the legacy template, you will need to explicitly use `-c v1` to select it.
+package_name = '{{ cookiecutter.python_package_name }}'
+valid_python_name = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+if not re.match(valid_python_name, package_name):
+    print(f"ERROR: '{package_name}' is not a valid Python module name.")
+    sys.exit(1)
 
-Please update any scripts/automation you have to append the `-c v1` option,
-which is available now.
-
-For example:
-    cookiecutter -c v1 https://github.com/drivendata/cookiecutter-data-science
-=============================================================================
-
-    """)
-
-
-deprecation_warning()
+print("Template looks ok.")
