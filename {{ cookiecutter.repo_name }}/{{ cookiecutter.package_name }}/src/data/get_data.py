6f6157787e119dbfeb0eef3a4476da1264ee07c0
Paul Steffen <46781050+PaulSteffen-betclic@users.noreply.github.com>
2023-10-02 09:52:54 +0200

    define .py script skeleton

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/src/data/get_data.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/src/data/get_data.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
+import os
+import hydra
+
 from dotenv import find_dotenv, load_dotenv
+from loguru import logger
+from omegaconf import DictConfig
+from utils import configure_logger, CONFIG_NAME, CONFIG_PATH
 
+@logger.catch
+@hydra.main(version_base=None, config_path=CONFIG_PATH, config_name=CONFIG_NAME)
+def main(cfg: DictConfig):
+    pass
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+if __name__ == "__main__":
+    configure_logger()
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    load_dotenv(find_dotenv(), override=True)
+    secrets_path = os.path.join(os.getcwd(), ".secrets")
+    load_dotenv(secrets_path, override=True)
 
     main()
