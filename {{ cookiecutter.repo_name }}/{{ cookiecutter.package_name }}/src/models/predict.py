a5bcef3d421704467f3d370f379d8171ccdebb27
Paul Steffen <46781050+PaulSteffen-betclic@users.noreply.github.com>
2023-10-02 09:53:52 +0200

    define .py script skeleton

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/src/models/predict.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import hydra
+
+from dotenv import find_dotenv, load_dotenv
+from loguru import logger
+from omegaconf import DictConfig
+from utils import configure_logger, CONFIG_NAME, CONFIG_PATH
+
+@logger.catch
+@hydra.main(version_base=None, config_path=CONFIG_PATH, config_name=CONFIG_NAME)
+def main(cfg: DictConfig):
+    pass
+
+if __name__ == "__main__":
+    configure_logger()
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv(), override=True)
+    secrets_path = os.path.join(os.getcwd(), ".secrets")
+    load_dotenv(secrets_path, override=True)
+
+    main()
