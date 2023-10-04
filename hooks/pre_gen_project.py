6d17f317b126f375cd7edb612958d412b96b9d71
Paul Steffen <46781050+PaulSteffen-betclic@users.noreply.github.com>
2023-10-04 11:25:54 +0200

    use hyphens instead of underscores in package name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -13,7 +13,7 @@ print(logo)
 print("Validation of template values...")
 
 package_name = '{{ cookiecutter.package_name }}'
-valid_python_name = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+valid_python_name = r'^[-a-zA-Z][-a-zA-Z0-9]+$'
 if not re.match(valid_python_name, package_name):
     print(f"ERROR: '{package_name}' is not a valid Python module name.")
     sys.exit(1)
